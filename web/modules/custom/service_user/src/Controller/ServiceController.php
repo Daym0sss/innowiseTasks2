23779cb5b9bef77ac52066a34f6b46e93bf1fb15
Daym0sss <lavandos322@gmail.com>
2022-11-14 15:16:29 +0300

    Add module

--- web/modules/custom/service_user/src/Controller/ServiceController.php
+++ web/modules/custom/service_user/src/Controller/ServiceController.php
@@ -1,1 +1,42 @@
+<?php
 
+namespace Drupal\service_user\Controller;
+
+class ServiceController
+{
+    public function mainPage(): array
+    {
+        /*$params=array(
+          'id' => '4924',
+          'name' => 'Ivan Lodnya',
+          'email' => 'lavandos322@gmail.com',
+          'gender' => 'male',
+          'status' => 'inactive'
+        );
+        $params=array(
+          'id' => '4924'
+        );*/
+
+        $response=\Drupal::service('service_user.user_service')->getList();
+        $obj_arr=json_decode($response);
+        $result_arr=array();
+
+        foreach ($obj_arr as $obj)
+        {
+            $temp=array();
+
+            $temp['id']=$obj->{'id'};
+            $temp['name']=$obj->{'name'};
+            $temp['email']=$obj->{'email'};
+            $temp['gender']=$obj->{'gender'};
+            $temp['status']=$obj->{'status'};
+
+            $result_arr[] = $temp;
+        }
+
+        return [
+            '#theme' => 'main_template',
+            '#data' => $result_arr,
+        ];
+    }
+}
--- web/modules/custom/service_user/src/services/UserServiceObject.php
+++ web/modules/custom/service_user/src/services/UserServiceObject.php
@@ -1,1 +1,53 @@
+<?php
 
+namespace Drupal\service_user\services;
+use Drupal\service_user\interfaces\IRequest;
+use GuzzleHttp;
+
+class UserServiceObject implements IRequest
+{
+    private $uri="https://gorest.co.in/public/v2/users";
+    private $client;
+    private $token="7e8c447ccef10ba84d23ffb9d3272e8ae8cc8869b77724ad050c3af4130fcb29";
+
+    public function __construct()
+    {
+        $this->client=new GuzzleHttp\Client();
+    }
+
+    public function getList(): string
+    {
+        $result=$this->client->request('GET',$this->uri . "?access-token=" . $this->token);
+        return $result->getBody()->getContents();
+    }
+
+    public function createUser($parameters)
+    {
+        $result=$this->client->request('POST',$this->uri . "?access-token=$this->token",[
+            'json' => ['name' => $parameters['name'],'email' => $parameters['email'], 'gender' => $parameters['gender'], 'status' => $parameters['status']]
+        ]);
+    }
+
+    public function deleteUser($parameters)
+    {
+        $id=$parameters['id'];
+
+        $result=$this->client->request('DELETE',$this->uri . "/$id?access-token=$this->token");
+    }
+
+    public function updateUser($parameters)
+    {
+        $id=$parameters['id'];
+
+        $result=$this->client->request('PATCH',$this->uri . "/$id?access-token=$this->token",[
+            'json' => ['name' => $parameters['name'],'email' => $parameters['email'], 'gender' => $parameters['gender'], 'status' => $parameters['status']]
+        ]);
+    }
+
+    public function getUserDetails($parameters)
+    {
+        $id=$parameters['id'];
+        $result=$this->client->request('GET',$this->uri . "/$id?access-token=$this->token");
+        return $result->getBody()->getContents();
+    }
+}
