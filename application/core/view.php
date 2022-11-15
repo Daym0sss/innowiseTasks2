15f430353c07388d101560117c8a652148781c6b
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-11-16 00:37:40 +0300

    Delete application directory

--- application/core/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class View
-{
-    public function generate($content_view, $template_view, $data=null)
-    {
-        include $_SERVER['DOCUMENT_ROOT'] . "/task1/application/view/" . $template_view;
-    }
-}
