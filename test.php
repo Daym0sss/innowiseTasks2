9ad7086c5ff5ab4532e3140188bf2eef26a93844
Daym0sss <lavandos322@gmail.com>
2023-01-03 15:24:34 +0300

    Dockerised app files

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require $_SERVER['DOCUMENT_ROOT'] . '/tasks/task2/system/Database.php';
-    require $_SERVER['DOCUMENT_ROOT'] . '/tasks/task2/system/IRequest.php';
-    require $_SERVER['DOCUMENT_ROOT'] . '/tasks/task2/database/REST_API.php';
-
-    $handler = REST_API::getInstance();
-    var_dump($handler->create('Ivan Lodnya', 'lavandos322@gmail.com','male','active'));
